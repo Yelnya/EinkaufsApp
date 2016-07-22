8b01c743774ea1a694af102f5d4b0867ce68febd
Yelnya <vypjordan@gmail.com>
2016-07-22 12:34:43 +0200

    Gradle, TestFragment, Layout, TestAdapter ~ test instances for expandable RecyclerView

---
+++ app/src/main/java/com/examples/pj/einkaufsapp/adapters/TestAdapter.java
@@ -0,0 +1,157 @@
+package com.examples.pj.einkaufsapp.adapters;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.examples.pj.einkaufsapp.R;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+public class TestAdapter extends BaseAdapter<TestAdapter.ListHeaderViewHolder> {
+
+    public static final int HEADER = 0;
+    public static final int CHILD = 1;
+
+    private List<Item> data;
+    private Context context;
+    private boolean editDeleteToolbarActive;
+
+    public TestAdapter(Context context, List<Item> data) {
+        this.data = data;
+        this.context = context;
+    }
+
+    //---------------------------------------------------------------
+    // Create new views (invoked by the layout manager)
+    //---------------------------------------------------------------
+
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int type) {
+        View view = null;
+        float dp = context.getResources().getDisplayMetrics().density;
+        int subItemPaddingLeft = (int) (18 * dp);
+        int subItemPaddingTopAndBottom = (int) (5 * dp);
+        switch (type) {
+            case HEADER:
+                LayoutInflater inflater = (LayoutInflater) parent.getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                view = inflater.inflate(R.layout.list_header, parent, false);
+                ListHeaderViewHolder header = new ListHeaderViewHolder(view);
+                return header;
+            case CHILD:
+                TextView itemTextView = new TextView(context);
+                itemTextView.setPadding(subItemPaddingLeft, subItemPaddingTopAndBottom, 0, subItemPaddingTopAndBottom);
+                itemTextView.setTextColor(0x88000000);
+                itemTextView.setLayoutParams(
+                        new ViewGroup.LayoutParams(
+                                ViewGroup.LayoutParams.MATCH_PARENT,
+                                ViewGroup.LayoutParams.WRAP_CONTENT));
+                return new RecyclerView.ViewHolder(itemTextView) {
+                };
+        }
+        return null;
+    }
+
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+        final Item item = data.get(position);
+        switch (item.type) {
+            case HEADER:
+                final ListHeaderViewHolder itemController = (ListHeaderViewHolder) holder;
+                itemController.refferalItem = item;
+                itemController.header_title.setText(item.text);
+                if (item.invisibleChildren == null) {
+                    itemController.btn_expand_toggle.setImageResource(R.drawable.circle_minus);
+                } else {
+                    itemController.btn_expand_toggle.setImageResource(R.drawable.circle_plus);
+                }
+                itemController.btn_expand_toggle.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        if (item.invisibleChildren == null) {
+                            item.invisibleChildren = new ArrayList<Item>();
+                            int count = 0;
+                            int pos = data.indexOf(itemController.refferalItem);
+                            while (data.size() > pos + 1 && data.get(pos + 1).type == CHILD) {
+                                item.invisibleChildren.add(data.remove(pos + 1));
+                                count++;
+                            }
+                            notifyItemRangeRemoved(pos + 1, count);
+                            itemController.btn_expand_toggle.setImageResource(R.drawable.circle_plus);
+                        } else {
+                            int pos = data.indexOf(itemController.refferalItem);
+                            int index = pos + 1;
+                            for (Item i : item.invisibleChildren) {
+                                data.add(index, i);
+                                index++;
+                            }
+                            notifyItemRangeInserted(pos + 1, index - pos - 1);
+                            itemController.btn_expand_toggle.setImageResource(R.drawable.circle_minus);
+                            item.invisibleChildren = null;
+                        }
+                    }
+                });
+                break;
+            case CHILD:
+                TextView itemTextView = (TextView) holder.itemView;
+                itemTextView.setText(data.get(position).text);
+                break;
+        }
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        return data.get(position).type;
+    }
+
+    @Override
+    public int getItemCount() {
+        return data.size();
+    }
+
+    //---------------------------------------------------------------
+    // INNER CLASSES
+    //---------------------------------------------------------------
+
+    public static class ListHeaderViewHolder extends RecyclerView.ViewHolder {
+        public Item refferalItem;
+        @Bind(R.id.header_title)
+        TextView header_title;
+        @Bind(R.id.btn_expand_toggle)
+        ImageView btn_expand_toggle;
+
+        public ListHeaderViewHolder(View itemView) {
+            super(itemView);
+            ButterKnife.bind(this, itemView);
+        }
+    }
+
+    public static class Item {
+        public int type;
+        public String text;
+        public List<Item> invisibleChildren;
+
+        public Item() {
+        }
+
+        public Item(int type, String text) {
+            this.type = type;
+            this.text = text;
+        }
+    }
+
+    //---------------------------------------------------------------
+    // GETTER AND SETTER
+    //---------------------------------------------------------------
+
+    public void setEditDeleteToolbarActive(boolean editDeleteToolbarActive) {
+        this.editDeleteToolbarActive = editDeleteToolbarActive;
+    }
+}
---
+++ app/src/main/java/com/examples/pj/einkaufsapp/fragments/TestFragment.java
@@ -0,0 +1,183 @@
+package com.examples.pj.einkaufsapp.fragments;
+
+import android.app.Activity;
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+
+import com.examples.pj.einkaufsapp.R;
+import com.examples.pj.einkaufsapp.adapters.TestAdapter;
+import com.examples.pj.einkaufsapp.dbentities.ShoppingTrip;
+import com.examples.pj.einkaufsapp.interfaces.ChangeToolbarInterface;
+import com.examples.pj.einkaufsapp.util.SharedPreferencesManager;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.Bind;
+
+public class TestFragment extends BaseFragment implements ChangeToolbarInterface {
+    public static final String LOG_TAG = TestFragment.class.getSimpleName();
+
+    Context context;
+    SharedPreferencesManager sharedPreferencesManager;
+    TestAdapter testAdapter;
+    private List<ShoppingTrip> historicShoppingTripsList;
+    private boolean showEditAndDeleteIconInToolbar;
+    private String toolbarTitle = "";
+    private static final String TOOLBAR_TITLE_FRAGMENT = "Test";
+    private static final String TOOLBAR_TITLE_EDIT = "Löschen/Ändern";
+
+    @Bind(R.id.test_recycler_view)
+    RecyclerView testRv;
+    @Bind(R.id.toolbarDeleteIv)
+    ImageView toolbarDeleteIv;
+    @Bind(R.id.toolbarEditIv)
+    ImageView toolbarEditIv;
+
+    //================================================================================
+    // Fragment Instantiation
+    //================================================================================
+
+    /**
+     * Constructor
+     */
+    public TestFragment() {
+        super(LOG_TAG, true);   //influences Hamburger Icon HomeUp in Toolbar
+    }
+
+    /**
+     * createInstance for getting arguments in bundle
+     *
+     * @return fragment
+     */
+    public static BaseFragment createInstance() {
+        final BaseFragment fragment = new TestFragment();
+        final Bundle args = new Bundle();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    //================================================================================
+    // Base Methods
+    //================================================================================
+
+    @Override
+    protected int getLayoutId() {
+        return R.layout.fragment_test;
+    }
+
+    @Override
+    protected void setToolbar() {
+        getAttachedActivity().setToolbar(toolbar, true, toolbarTitle, showEditAndDeleteIconInToolbar); //Icon displayed, Titel of Toolbar
+    }
+
+    @Override
+    protected void setToolbarEditAndDeleteIcon(boolean showEditAndDeleteIconInToolbar) {
+        toolbarEditIv.setVisibility(showEditAndDeleteIconInToolbar ? View.VISIBLE : View.INVISIBLE);
+        toolbarDeleteIv.setVisibility(showEditAndDeleteIconInToolbar ? View.VISIBLE : View.INVISIBLE);
+        toolbarTv.setText(toolbarTitle);
+    }
+
+    @Override
+    public boolean canGoBack() {
+        return true;
+    }
+
+    //================================================================================
+    // Fragment Lifecycle
+    //================================================================================
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        showEditAndDeleteIconInToolbar = false;
+
+        context = getActivity();
+        if (sharedPreferencesManager == null) {
+            sharedPreferencesManager = SharedPreferencesManager.initSharedPreferences((Activity) context);
+        }
+        historicShoppingTripsList = sharedPreferencesManager.loadHistoricShoppingTripsListFromLocalStore();
+        showEditAndDeleteIconInToolbar = false;
+
+        initializeRvAdapter();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        toolbarTitle = TOOLBAR_TITLE_FRAGMENT;
+        setToolbarEditAndDeleteIcon(showEditAndDeleteIconInToolbar);
+        context = getActivity();
+        if (sharedPreferencesManager == null) {
+            sharedPreferencesManager = SharedPreferencesManager.initSharedPreferences((Activity) context);
+        }
+        historicShoppingTripsList = sharedPreferencesManager.loadHistoricShoppingTripsListFromLocalStore();
+    }
+
+    //---------------------------------------------------------------
+    // Other Methods
+    //---------------------------------------------------------------
+
+    private void toolbarBackToNormal() {
+        showEditAndDeleteIconInToolbar = false;
+        toolbarTitle = TOOLBAR_TITLE_FRAGMENT;
+        setToolbarEditAndDeleteIcon(showEditAndDeleteIconInToolbar);
+        testAdapter.setEditDeleteToolbarActive(false);
+    }
+
+    private void initializeRvAdapter() {
+        List<TestAdapter.Item> data = new ArrayList<>();
+
+        data.add(new TestAdapter.Item(TestAdapter.HEADER, "Fruits"));
+        data.add(new TestAdapter.Item(TestAdapter.CHILD, "Apple"));
+        data.add(new TestAdapter.Item(TestAdapter.CHILD, "Orange"));
+        data.add(new TestAdapter.Item(TestAdapter.CHILD, "Banana"));
+        data.add(new TestAdapter.Item(TestAdapter.HEADER, "Cars"));
+        data.add(new TestAdapter.Item(TestAdapter.CHILD, "Audi"));
+        data.add(new TestAdapter.Item(TestAdapter.CHILD, "Aston Martin"));
+        data.add(new TestAdapter.Item(TestAdapter.CHILD, "BMW"));
+        data.add(new TestAdapter.Item(TestAdapter.CHILD, "Cadillac"));
+
+        TestAdapter.Item places = new TestAdapter.Item(TestAdapter.HEADER, "Places");
+        places.invisibleChildren = new ArrayList<>();
+        places.invisibleChildren.add(new TestAdapter.Item(TestAdapter.CHILD, "Kerala"));
+        places.invisibleChildren.add(new TestAdapter.Item(TestAdapter.CHILD, "Tamil Nadu"));
+        places.invisibleChildren.add(new TestAdapter.Item(TestAdapter.CHILD, "Karnataka"));
+        places.invisibleChildren.add(new TestAdapter.Item(TestAdapter.CHILD, "Maharashtra"));
+        data.add(places);
+
+        //get shoppingTrips from list -> data
+//        historicShoppingTripsList
+
+        //get productItems from shoppingTrips from list -> places
+
+
+        testAdapter = new TestAdapter(context, data);
+        final LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getActivity());
+        linearLayoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+        testRv.setLayoutManager(linearLayoutManager);
+        testRv.setAdapter(testAdapter);
+        testRv.setHasFixedSize(true);
+        testRv.setVisibility(View.VISIBLE);
+    }
+
+    //---------------------------------------------------------------
+    // ChangeToolbarInterface Methods
+    //---------------------------------------------------------------
+
+    @Override
+    public void showEditAndDeleteIcon(boolean show) {
+        // change Toolbar
+        Log.d(LOG_TAG, "Show Edit and Delete Icons in Toolbar: " + show);
+        toolbarTitle = show ? TOOLBAR_TITLE_EDIT : TOOLBAR_TITLE_FRAGMENT;
+        showEditAndDeleteIconInToolbar = show;
+        setToolbarEditAndDeleteIcon(show);
+        setToolbar();
+    }
+}

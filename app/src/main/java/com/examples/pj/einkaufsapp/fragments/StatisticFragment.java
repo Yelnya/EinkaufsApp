aae50a95d54e6e14022fc672054421f93eb90227
Yelnya <vypjordan@gmail.com>
2016-07-28 16:45:00 +0200

    Statistic Fragment ~ little cleanup

--- app/src/main/java/com/examples/pj/einkaufsapp/fragments/StatisticFragment.java
+++ app/src/main/java/com/examples/pj/einkaufsapp/fragments/StatisticFragment.java
@@ -1,24 +1,17 @@
 package com.examples.pj.einkaufsapp.fragments;
 
-import android.app.Activity;
 import android.content.Context;
 import android.os.Bundle;
-import android.text.TextUtils;
-import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
-import android.widget.EditText;
 import android.widget.ListView;
-import android.widget.Spinner;
 import android.widget.TextView;
 
 import com.examples.pj.einkaufsapp.R;
 import com.examples.pj.einkaufsapp.dbentities.ProductItem;
 import com.examples.pj.einkaufsapp.dbentities.ProductItemDataSource;
-import com.examples.pj.einkaufsapp.util.ViewUtils;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -34,7 +27,6 @@ public class StatisticFragment extends BaseFragment {
     private static final String TOOLBAR_TITLE = "Statistiken";
     private ProductItemDataSource dataSource;
     private ListView mProductItemListView;
-    private String selectedCategory;
     private boolean showEditAndDeleteIconInToolbar;
     private boolean showShoppingCartIconInToolbar;
     //================================================================================
@@ -86,7 +78,7 @@ public class StatisticFragment extends BaseFragment {
 
     @Override
     protected void onCleanUp() {
-
+        //not needed
     }
 
     //================================================================================
@@ -98,34 +90,26 @@ public class StatisticFragment extends BaseFragment {
         super.onActivityCreated(savedInstanceState);
 
         context = this.getActivity();
-
-        Log.d(LOG_TAG, "Das Datenquellen-Objekt wird angelegt.");
+        showShoppingCartIconInToolbar = false;
+        showEditAndDeleteIconInToolbar = false;
         dataSource = new ProductItemDataSource(context);
 
         initializeProductItemsListView();
-        activateAddButton();    // + Button
     }
 
     @Override
     public void onPause() {
         super.onPause();
-        Log.d(LOG_TAG, "Die Datenquelle wird geschlossen.");
         dataSource.close(); //close db connection
     }
 
     @Override
     public void onResume() {
         super.onResume();
-        showShoppingCartIconInToolbar = false;
-        showEditAndDeleteIconInToolbar = false;
-        setToolbarEditAndDeleteIcon(showEditAndDeleteIconInToolbar);
-        Log.d(LOG_TAG, "Die Datenquelle wird geöffnet.");
-        dataSource.open();  //Verbindung zur Datenbank geöffnet
-        Log.d(LOG_TAG, "Folgende Einträge sind in der Datenbank vorhanden:");
-        showAllListEntries();
+        dataSource.open();  //open db connection
     }
 
-    //holen einer Liste mit allen Einträgen aus der DB
+    //
     private void showAllListEntries() {
         List<ProductItem> productItemList = dataSource.getAllProductItems();
 
@@ -136,64 +120,20 @@ public class StatisticFragment extends BaseFragment {
         adapter.notifyDataSetChanged();
     }
 
-    //Beim Drücken des + Buttons
-    private void activateAddButton() {
-        Button buttonAddProduct = (Button) getActivity().findViewById(R.id.button_add_product);
-        final EditText editTextProduct = (EditText) getActivity().findViewById(R.id.autocompletetv_edittext_product);
 
-        final Spinner spinner = (Spinner) getActivity().findViewById(R.id.spinner_category);
-        // Create an ArrayAdapter using the string array and a default spinner layout
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(context,
-                R.array.categories_array, R.layout.spinner_item);
-        // Specify the layout to use when the list of choices appears
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        // Apply the adapter to the spinner
-        spinner.setAdapter(adapter);
-        spinner.setSelection(0);
-        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                selectedCategory = parent.getItemAtPosition(position).toString();
-            }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> parent) {
-                //do nothing
-            }
-        });
-
-        buttonAddProduct.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String product = editTextProduct.getText().toString();
-                if (TextUtils.isEmpty(product)) {
-                    editTextProduct.setError(getString(R.string.editText_errorMessage));
-                    return;
-                }
-                editTextProduct.setText("");
-
-                dataSource.createProductItem(product, selectedCategory);
-                ViewUtils.hideKeyboard((Activity) context);
-
-                showAllListEntries();   //Anzeigen aller Datenbank Einträge in der ListView
-            }
-        });
-    }
-
-
-    //Initialisierung der ListView
+    //initialization of list view
     private void initializeProductItemsListView() {
         List<ProductItem> emptyListForInitialization = new ArrayList<>();
 
         mProductItemListView = (ListView) getActivity().findViewById(R.id.listview_product_items);
 
-        // Erstellen des ArrayAdapters für der ListView
+        // create array adapter for list view
         ArrayAdapter<ProductItem> productItemArrayAdapter = new ArrayAdapter<ProductItem>(
                 getActivity(),
                 android.R.layout.simple_list_item_multiple_choice,
                 emptyListForInitialization) {
 
-            // Wird immer dann aufgerufen, wenn der übergeordnete ListView die Zeile neu zeichnen muss
+            // call if line has to be drawn new
             @Override
             public View getView(int position, View convertView, ViewGroup parent) {
 
@@ -214,8 +154,7 @@ public class StatisticFragment extends BaseFragment {
             public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
                 ProductItem productItem = (ProductItem) adapterView.getItemAtPosition(position);
 
-                // Hier den checked-Wert des ProductItem-Objekts umkehren, bspw. von true auf false
-                // Dann ListView neu zeichnen mit showAllListEntries()
+                // invert checked value for productItem, then draw list new
                 dataSource.updateProductItem(productItem.getId(), productItem.getProduct(), productItem.getCategory(), productItem.getBought(), !productItem.isDone(), productItem.isFavourite());
                 showAllListEntries();
             }
